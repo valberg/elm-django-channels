2e3cd5833cc14f26a39ed5eb19f0d970a099c36c
Vidir Valberg Gudmundsson <valberg@orn.li>
2017-12-08 00:25:45 +0100

    Add elm-decode-pipepline and docs.

--- src/DjangoChannels.elm
+++ src/DjangoChannels.elm
@@ -1,4 +1,21 @@
-module DjangoChannels exposing (ModelBindingPayload, modelBindingDecoder, streamDecoder)
+module DjangoChannels
+    exposing
+        ( ModelBindingPayload
+        , modelBindingDecoder
+        , streamDecoder
+        )
+
+{-|
+
+Decoders and such to ease communication with django channels.
+
+# Definition
+@docs ModelBindingPayload
+
+# Decoders
+@docs modelBindingDecoder, streamDecoder
+
+-}
 
 -- Core modules
 
@@ -10,6 +27,10 @@ import Json.Decode exposing (Decoder, string, field)
 import Json.Decode.Pipeline exposing (decode, requiredAt)
 
 
+{-| ModelBindingPayload has the shape of what is sent by channels when a
+    WebsocketBinding is triggered.
+
+-}
 type alias ModelBindingPayload dataType pkType =
     { model : String
     , data : dataType
@@ -18,6 +39,30 @@ type alias ModelBindingPayload dataType pkType =
     }
 
 
+{-| Decode data sent from a channels WebsocketBinding.
+
+    import Json.Decode exposing (Decoder, string, bool)
+    import Json.Decode.Pipeline exposing (decode, required)
+
+    type alias TodoItem =
+        { pk : String
+        , description : String
+        , isDone : Bool
+        }
+
+    postBindingDecoder : Decoder (ModelBindingPayload Post String)
+    postBindingDecoder =
+        let
+            todoItemDecoder : Decoder TodoItem
+            todoItemDecoder =
+                decode TodoItem
+                |> required "pk" string
+                |> required "description" string
+                |> required "is_done" bool
+        in
+            modelBindingDecoder todoItemDecoder string
+
+-}
 modelBindingDecoder : Decoder a -> Decoder b -> Decoder (ModelBindingPayload a b)
 modelBindingDecoder dataDecoder pkDecoder =
     decode ModelBindingPayload
@@ -27,6 +72,8 @@ modelBindingDecoder dataDecoder pkDecoder =
         |> requiredAt [ "payload", "action" ] string
 
 
+{-| Deal with streams from a WebsocketDemultiplexer.
+-}
 streamDecoder : String -> (String -> model) -> model -> model
 streamDecoder message okFunc fallback =
     case Json.Decode.decodeString (field "stream" string) message of
