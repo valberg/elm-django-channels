496e28205b48f8ea21a23153c79f799ea8c49fda
Vidir Valberg Gudmundsson <valberg@orn.li>
2017-12-09 17:03:35 +0100

    Abstracting things a bit more.

--- src/DjangoChannels.elm
+++ src/DjangoChannels.elm
@@ -1,19 +1,27 @@
 module DjangoChannels
     exposing
-        ( ModelBindingPayload
-        , modelBindingDecoder
-        , streamDecoder
+        ( StreamHandler
+        , handleStream
+        , streamDemultiplexer
+        , defaultCreate
+        , defaultUpdate
+        , defaultDelete
         )
 
 {-|
 
 Decoders and such to ease communication with django channels.
 
-# Definition
-@docs ModelBindingPayload
+elm-django-channels is an oppinionated way to use communicate with Django Channels from Elm.
 
-# Decoders
-@docs modelBindingDecoder, streamDecoder
+# Definition
+@docs StreamHandler
+
+# Utilities
+@docs streamDemultiplexer, handleStream
+
+# Default operations
+@docs defaultCreate, defaultUpdate, defaultDelete
 
 -}
 
@@ -27,42 +35,14 @@ import Json.Decode exposing (Decoder, string, field)
 import Json.Decode.Pipeline exposing (decode, requiredAt)
 
 
-{-| ModelBindingPayload has the shape of what is sent by channels when a
-    WebsocketBinding is triggered.
-
--}
-type alias ModelBindingPayload dataType pkType =
+type alias ModelBindingPayload instanceType pkType =
     { model : String
-    , data : dataType
+    , data : instanceType
     , pk : pkType
     , action : String
     }
 
 
-{-| Decode data sent from a channels WebsocketBinding.
-
-    import Json.Decode exposing (Decoder, string, bool)
-    import Json.Decode.Pipeline exposing (decode, required)
-
-    type alias TodoItem =
-        { pk : String
-        , description : String
-        , isDone : Bool
-        }
-
-    postBindingDecoder : Decoder (ModelBindingPayload Post String)
-    postBindingDecoder =
-        let
-            todoItemDecoder : Decoder TodoItem
-            todoItemDecoder =
-                decode TodoItem
-                |> required "pk" string
-                |> required "description" string
-                |> required "is_done" bool
-        in
-            modelBindingDecoder todoItemDecoder string
-
--}
 modelBindingDecoder : Decoder a -> Decoder b -> Decoder (ModelBindingPayload a b)
 modelBindingDecoder dataDecoder pkDecoder =
     decode ModelBindingPayload
@@ -74,11 +54,82 @@ modelBindingDecoder dataDecoder pkDecoder =
 
 {-| Deal with streams from a WebsocketDemultiplexer.
 -}
-streamDecoder : String -> (String -> model) -> model -> model
-streamDecoder message okFunc fallback =
-    case Json.Decode.decodeString (field "stream" string) message of
+streamDemultiplexer : String -> String
+streamDemultiplexer data =
+    case Json.Decode.decodeString (field "stream" string) data of
         Ok stream ->
-            okFunc stream
+            stream
 
         Err _ ->
-            fallback
+            "nothing"
+
+
+{-| StreamHandler defines how we should deal with a specific stream.
+-}
+type alias StreamHandler instanceType pkType =
+    { streamName : String
+    , instanceDecoder : Decoder instanceType
+    , pkDecoder : Decoder pkType
+    , createFunc : instanceType -> pkType -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+    , updateFunc : instanceType -> pkType -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+    , deleteFunc : pkType -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+    }
+
+
+{-| Handle data on a given StreamHandler.
+-}
+handleStream : StreamHandler instanceType pkType -> String -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+handleStream streamHandler data instances =
+    let
+        decoder =
+            modelBindingDecoder streamHandler.instanceDecoder streamHandler.pkDecoder
+    in
+        case Json.Decode.decodeString decoder data of
+            Ok decodedData ->
+                case decodedData.action of
+                    "create" ->
+                        streamHandler.createFunc decodedData.data decodedData.pk instances
+
+                    "update" ->
+                        streamHandler.updateFunc decodedData.data decodedData.pk instances
+
+                    "delete" ->
+                        streamHandler.deleteFunc decodedData.pk instances
+
+                    _ ->
+                        instances
+
+            Err _ ->
+                instances
+
+
+
+-- Default create, update, delete functions
+
+
+{-| Default function for creating an instance.
+-}
+defaultCreate : instanceType -> pkType -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+defaultCreate instance pk instances =
+    ( pk, instance ) :: instances
+
+
+{-| Default function for updating an instance.
+-}
+defaultUpdate : instanceType -> pkType -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+defaultUpdate instance pk instances =
+    List.map
+        (\( curPk, x ) ->
+            if pk == curPk then
+                ( curPk, instance )
+            else
+                ( curPk, x )
+        )
+        instances
+
+
+{-| Default function for deleting an instance given a pk.
+-}
+defaultDelete : pkType -> List ( pkType, instanceType ) -> List ( pkType, instanceType )
+defaultDelete pk instances =
+    List.filter (\( curPk, _ ) -> pk /= curPk) instances
